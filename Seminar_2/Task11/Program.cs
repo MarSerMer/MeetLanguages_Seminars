28a342fd9703da515ddfb5bf9514eff9e98e8ec8
MarinaMerzliakova <marinakms@yandex.ru>
2022-09-02 21:24:09 +0300

    Task 11 added

---
+++ Seminar_2/Task11/Program.cs
@@ -0,0 +1,12 @@
+﻿// 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Random rand = new Random();
+int number = rand.Next(10, 100);
+Console.WriteLine($"Сгенерировано случайное число {number}");
+int digit3 = number%10;
+int digit1 = number / 100;
+int result = digit1*10 + digit3;
+Console.WriteLine(result);
---
+++ Seminar_2/Task12/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 12: Напишите программу, которая будет принимать на вход два числа и выводить, 
+//является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Writeline("Введите первое число (делимое)");
+int number1 = Convert.ToInt32(Console.Readline());
+Console.Writeline("Введите первое число (делитель)");
+int number2 = Convert.ToInt32(Console.Readline());
+
+int result = number1 % number2;
+if (result == 0)
+{
+    Console.Writeline($"Число {number1} делится на {number2} без остатка");
+}
+else
+{
+    Console.Writeline($"Число {number1} делится на {number2} с остатком {result}");
+}
